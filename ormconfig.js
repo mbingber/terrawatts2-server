71572ec26458acbe5ab51f0443b0971fcc4713cb
Mike Ingber <mbingber@gmail.com>
2020-03-29 17:10:25 -0400

    heroku postgres maybe

--- ormconfig.js
+++ ormconfig.js
@@ -1,25 +1,47 @@
 const dotenv = require("dotenv");
+const { parse } = require("pg-connection-string");
 dotenv.config();
 
+const isProd = !!process.env.DATABASE_URL;
+
+const connectionObj = {
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: "terrawatts2"
+};
+
+if (isProd) {
+   const databaseUrl = process.env.DATABASE_URL;
+   const connectionOptions = parse(databaseUrl);
+
+   connectionObj.name = connectionOptions.name;
+   connectionObj.host = connectionOptions.host;
+   connectionObj.post = connectionOptions.port;
+   connectionObj.username = connectionOptions.username;
+   connectionObj.password = connectionOptions.password;
+   connectionObj.database = connectionOptions.database;
+   connectionObj.extra = {
+      ssl: true
+   };
+}
+
 module.exports = {
-   "type": "postgres",
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "username": process.env.DB_USERNAME,
-   "password": process.env.DB_PASSWORD,
-   "database": "terrawatts2",
-   "synchronize": true,
-   "logging": false,
-   "entities": [
+   ...connectionObj,
+   type: "postgres",
+   synchronize: true,
+   logging: false,
+   entities: [
       "src/entity/**/*.ts"
    ],
-   "migrations": [
+   migrations: [
       "src/migration/**/*.ts"
    ],
-   "subscribers": [
+   subscribers: [
       "src/subscriber/**/*.ts"
    ],
-   "cli": {
+   cli: {
       "entitiesDir": "src/entity",
       "migrationsDir": "src/migration",
       "subscribersDir": "src/subscriber"
