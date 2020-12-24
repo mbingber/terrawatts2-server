fdce24e81ef0e3bc90a7ff1d1f867e3c254b3d65
Mike Ingber <mbingber@gmail.com>
2020-12-24 17:23:36 -0500

    no city plant market rule in china

--- src/logic/actions/cities.actions.ts
+++ src/logic/actions/cities.actions.ts
@@ -7,7 +7,11 @@ import { discardLowestPlant } from "./plants.actions";
 export const occupyCity = createAction<{ cityId: string; me: string }>('Occupy city');
 
 // if the max number of cities a player has exceeds the rank of a plant in the market, discard it
-export const enforceCityPlantMarketRule = (): Thunk => (dispatch, getState, { plantList }) => {
+export const enforceCityPlantMarketRule = (): Thunk => (dispatch, getState, { game, plantList }) => {
+  if (game.map.name === 'China') {
+    return;
+  }
+  
   const maxNumCities = selectMaxNumCities(getState());
   const lowestPlant = selectLowestPlantInMarket(getState(), { plantList });
 
