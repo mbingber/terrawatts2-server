4665c0cbf8e62fa6f0eb51cf58c5999148f34c31
Mike Ingber <mbingber@gmail.com>
2021-01-23 20:51:05 -0500

    comment out getStat

--- src/stats/getStat.ts
+++ src/stats/getStat.ts
@@ -1,22 +1,22 @@
-import { getStore, dispatchMove } from '../logic';
-import { AnyAction, Middleware } from '@reduxjs/toolkit';
-import { GameState } from '../logic/rootReducer';
-import { Context } from '../logic/types/thunks';
+// import { getStore, dispatchMove } from '../logic';
+// import { AnyAction, Middleware } from '@reduxjs/toolkit';
+// import { GameState } from '../logic/rootReducer';
+// import { Context } from '../logic/types/thunks';
 
-export type StatReducer<S> = (
-  acc: S,
-  gameState: GameState,
-  action: AnyAction,
-  context: Context
-) => S
+// export type StatReducer<S> = (
+//   acc: S,
+//   gameState: GameState,
+//   action: AnyAction,
+//   context: Context
+// ) => S
 
-export const getStat = <S>(context: Context, reducer: StatReducer<S>, initial: S): S => {
-  let result = initial;
-  const middleware: Middleware = s => next => action => {
-    result = reducer(result, s.getState(), action, context);
-    next(action);
-  }
-  const store = getStore(context, [middleware])
-  context.game.moves.forEach(move => dispatchMove(move, store));
-  return result;
-};
+// export const getStat = <S>(context: Context, reducer: StatReducer<S>, initial: S): S => {
+//   let result = initial;
+//   const middleware: Middleware = s => next => action => {
+//     result = reducer(result, s.getState(), action, context);
+//     next(action);
+//   }
+//   const store = getStore(context, [middleware])
+//   context.game.moves.forEach(move => dispatchMove(move, store));
+//   return result;
+// };
